566f8f99b9d976148afade6ea8714ceb5fd58d73
Srikanth-gunja <srikanthgunja25@gmail.com>
2024-03-20 12:55:28 +0530

    setting baseurl

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
   return (
     <>
    
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL} >
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
